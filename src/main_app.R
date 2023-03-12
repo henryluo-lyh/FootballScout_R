79518f3142dae94e8b34d1366bac72f9d8fcb965
bi9potato <bi9potato>
2023-03-11 18:37:07 -0800

    implment main_app

--- src/main_app.R
+++ src/main_app.R
@@ -8,6 +8,8 @@ library(plotly)
 
 library(tidyverse)
 
+source('./src/Panel1.R')
+
 # dataset
 data_add = './data/processed/all_players___.csv'
 df <- read_csv(data_add, col_names = TRUE)
@@ -98,6 +100,8 @@ app$layout(
           dbcTab(
             list(
               
+              get_panel1_content()
+              
             ), label = 'Panel1'
           ),
           
@@ -128,6 +132,259 @@ app$layout(
   
   
 )
+
+
+
+
+
+
+# callback
+
+# Panel1
+## dropdown2 need dropdown1 has a value firstly
+app$callback(
+  # output(id = 'p1_dd_team', 'options'),
+  list(
+    output(id = 'p1_dd_team', 'options'),
+    output(id = 'p1_dd_team', 'value')
+  ),
+  list(input(id = 'p1_dd_league', 'value')),
+  function(p1_dd_league_value) {
+    temp_league_ids <- list()
+    if (p1_dd_league_value == 'ALL') {
+      temp_league_ids <- league_ids
+    } else {
+      temp_league_ids <- p1_dd_league_value
+    }
+    
+    df_selected <- df[df$league_id %in% temp_league_ids,]
+    specified_teams <- unique(df_selected$team_name)
+    
+    options <- c(list(list(label = "ALL", value = "ALL") ), lapply(specified_teams, function(team_name) list(label = team_name, value = team_name_id[[team_name]])))
+    
+    return(list(options, NULL) )
+  }
+)
+
+# Panel1 - plot1
+app %>% add_callback(
+  output(id = 'p1_Iframe_1', 'figure'),
+  list(
+    input(id = 'p1_rs_year', 'value'),
+    input(id = 'p1_dd_league', 'value'),
+    input(id = 'p1_dd_team', 'value')
+  ),
+  function(p1_rs_year_value, p1_dd_league_value, p1_dd_team_value) {
+    # print(class( p1_rs_year_value[[1]]) )
+    
+    temp_league_ids <- c()
+    if (p1_dd_league_value == 'ALL') {
+      temp_league_ids <- league_ids # 是向量
+    } else {
+      temp_league_ids <- c(p1_dd_league_value)
+    }
+    
+    temp_team_ids <- c()
+    if (p1_dd_team_value == 'ALL') {
+      temp_team_ids <- unname(unlist(unique(df %>%
+                                              filter(league_id %in% temp_league_ids) %>%
+                                              select(team_id))) )
+    } else {
+      temp_team_ids <- c(p1_dd_team_value)
+    }
+    
+    years <- seq(p1_rs_year_value[[1]], p1_rs_year_value[[2]], by = 1)
+    # print(class(years) )
+    
+    df_selected <- df %>%
+      filter(team_id %in% temp_team_ids,
+             league_id %in% temp_league_ids,
+             league_season %in% years)
+    # print(head(df_selected))
+    
+    # get top 5 ids
+    df_mean <- df_selected %>%
+      group_by(player_id) %>%
+      summarise(games_rating = mean(games_rating, na.rm = TRUE)) %>%
+      ungroup() %>%
+      arrange(desc(games_rating)) %>%
+      slice_head(n = 5)
+    
+    
+    top5_ids <- df_mean$player_id
+    
+    # get
+    df_target <- df_selected %>%
+      filter(player_id %in% top5_ids)
+    # print(df_target)
+    
+    if( is.null(temp_league_ids[[1]]) | is.null(temp_team_ids[[1]]) |
+        nrow(df_target) == 0 ) {
+      return(ggplotly(ggplot() + theme_void()))
+    }
+    
+    chart1 <- ggplot(df_target, aes(x = league_season, y = games_rating, color = player_name)) +
+      geom_line() +
+      geom_point() +
+      scale_x_continuous(breaks = seq(2010, 2022, 1)) +
+      # scale_y_continuous(name = 'Game Rating', expand = expansion(mult = c(0, 0.05))) +
+      labs(title = 'Games Rating by Season and Player', x = 'Season', y = 'Games Rating')
+    
+    return(ggplotly(chart1) )
+  }
+)
+
+
+# Panel1 - plot2
+app %>% add_callback(
+  output(id = 'p1_Iframe_2', 'figure'),
+  list(
+    input(id = 'p1_rs_year', 'value'),
+    input(id = 'p1_dd_league', 'value'),
+    input(id = 'p1_dd_team', 'value')
+  ),
+  function(p1_rs_year_value, p1_dd_league_value, p1_dd_team_value) {
+    # drop na
+    df_dropna <- df %>%
+      drop_na(games_rating, league_season)
+    
+    # args
+    years <- p1_rs_year_value[[1]] : p1_rs_year_value[[2]]
+    
+    temp_league_ids <- ifelse(p1_dd_league_value == 'ALL', league_ids, p1_dd_league_value )
+    
+    temp_team_ids <- ifelse(p1_dd_team_value == 'ALL', 
+                            df %>%
+                              filter(league_id %in% temp_league_ids) %>%
+                              pull(team_id) %>%
+                              unique(), 
+                            p1_dd_team_value)
+    
+    # df_selected = df_dropna %>% filter(league_season %in% years)
+    df_selected <- df %>% 
+      filter(team_id %in% temp_team_ids, 
+             league_id %in% temp_league_ids, 
+             league_season %in% years) %>% 
+      select(league_season, games_rating) %>% 
+      na.omit()
+    
+    if( is.null(temp_league_ids[[1]]) | is.null(temp_team_ids[[1]]) |
+        nrow(df_selected) == 0 ) {
+      return(ggplotly(ggplot() + theme_void()))
+    }
+    
+    chart2 <- ggplot(df_selected, aes(x = games_rating)) + 
+      geom_density(fill = "skyblue") +
+      labs(title = 'Density Plot of Game Rating', x = 'Game Rating', y = 'Density')
+    
+    return(ggplotly(chart2) )
+  }
+)
+
+
+# Panel1 - plot3
+app %>% add_callback(
+  output(id = 'p1_Iframe_3', 'figure'),
+  list(
+    input(id = 'p1_rs_year', 'value'),
+    input(id = 'p1_dd_league', 'value'),
+    input(id = 'p1_dd_team', 'value')
+  ),
+  function(p1_rs_year_value, p1_dd_league_value, p1_dd_team_value) {
+    
+    # drop na
+    df_dropna <- df[!is.na(df$games_rating) & !is.na(df$league_season),]
+    
+    # args
+    years <- p1_rs_year_value[[1]]:p1_rs_year_value[[2]]
+    
+    if (p1_dd_league_value == 'ALL') {
+      temp_league_ids <- league_ids
+    } else {
+      temp_league_ids <- c(p1_dd_league_value)
+    }
+    
+    if (p1_dd_team_value == 'ALL') {
+      temp_team_ids <- unique(df[df$league_id %in% temp_league_ids,]$team_id)
+    } else {
+      temp_team_ids <- c(p1_dd_team_value)
+    }
+    
+    # df_selected <- df_dropna[df_dropna$league_season %in% years,]
+    df_selected <- df_dropna[df_dropna$team_id %in% temp_team_ids & df_dropna$league_id %in% temp_league_ids & df_dropna$league_season %in% years,]
+    
+    if( is.null(temp_league_ids[[1]]) | is.null(temp_team_ids[[1]]) |
+        nrow(df_selected) == 0 ) {
+      return(ggplotly(ggplot() + theme_void()))
+    }
+    
+    chart3 <- ggplot(df_selected, aes(x=games_rating)) +
+      geom_histogram(bins=200) +
+      scale_y_log10() +
+      labs(title = "Histogram of Game Rating", x = "Game Rating", y = "Count")
+    
+    return(ggplotly(chart3))
+    
+  }
+)
+
+# Panel1 - plot4
+app %>% add_callback(
+  output(id = 'p1_Iframe_4', 'figure'),
+  list(
+    input(id = 'p1_rs_year', 'value'),
+    input(id = 'p1_dd_league', 'value'),
+    input(id = 'p1_dd_team', 'value')
+  ),
+  function(p1_rs_year_value, p1_dd_league_value, p1_dd_team_value) {
+    
+    # args
+    years <- p1_rs_year_value[[1]]:p1_rs_year_value[[2]]
+    
+    if (p1_dd_league_value == "ALL") {
+      temp_league_ids <- league_ids
+    } else {
+      temp_league_ids <- c(p1_dd_league_value)
+    }
+    
+    if (p1_dd_team_value == "ALL") {
+      temp_team_ids <- df %>%
+        filter(league_id %in% temp_league_ids) %>%
+        select(team_id) %>%
+        distinct() %>%
+        pull()
+    } else {
+      temp_team_ids <- c(p1_dd_team_value)
+    }
+    
+    # df_selected <- df %>% 
+    #   filter(league_season %in% years)
+    df_selected <- df %>% 
+      filter(team_id %in% temp_team_ids, 
+             league_id %in% temp_league_ids, 
+             league_season %in% years) %>% 
+      select(league_season, games_rating) %>% 
+      na.omit()
+    
+    
+    if( is.null(temp_league_ids[[1]]) | is.null(temp_team_ids[[1]]) |
+        nrow(df_selected) == 0 ) {
+      return(ggplotly(ggplot() + theme_void()))
+    }
+    
+    chart4 <- ggplot(df_selected, aes(x = as.factor(league_season), y = games_rating)) +
+      geom_boxplot() +
+      labs(title = "Boxplot of Game Rating by Season", x = "League Season", y = "Game Rating") + 
+      coord_flip()
+    
+    return(ggplotly(chart4))
+    
+  }
+)
+
+
+
+
 
 
 
